696a56ba6d7518cac57a3e75bb0e013608c63303
Jaymez_S <james.shaojiaxuan@gmail.com>
2025-03-22 03:42:55 -0400

    Added transition between pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Landingpage from "./pages/Landingpage";
 import "./hideScrollbar.css"
-
+import { PageWrapper } from "./components/PageWrapper";
+import { AnimatePresence } from "framer-motion";
 const App = () => {
    return (
       <Router>
-         <div className="app">
+         <div className="app bg-neutral-900">
             <Navbar></Navbar>
-            <Routes>
-               <Route path="/" element={<Landingpage />} />
-               <Route path="/Home" element={<Homepage/>} />
-            </Routes>
+            <AnimatedRoutes/>  
          </div>
       </Router>
    );
 };
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<PageWrapper><Landingpage /></PageWrapper>} />
+        <Route path="/Home" element={<PageWrapper>< Homepage/></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 
 export default App;
