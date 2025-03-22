5ba441389fc97a66d19217fd00fdd15908a9478f
Jaymez_S <james.shaojiaxuan@gmail.com>
2025-03-22 17:57:59 -0400

    fixed testall code

--- backend/testall.py
+++ backend/testall.py
@@ -2,9 +2,22 @@ from test_script import test_script
 import json
 
 def test_all(jsonarray):
-  for item in jsonarray:
+  incorrectCases = []
+  for itemJson in jsonarray:
+    item = json.loads(itemJson)
+    comm = item["algorithm"]
+    opt = item["solution"]
+    subopt = item["incorrect_solution"]
 
-    comm = item.command
-    opt = item.optimal_solution
-    subopt = item.suboptimal_solution
+    result = test_script(opt)
+    if (not result["success"]):
+      print("answer for case: " + comm + " is incorrect: fix it in the future")
+      incorrectCases.append(item)
+
+  return incorrectCases
+  
+
+
+
+
 
